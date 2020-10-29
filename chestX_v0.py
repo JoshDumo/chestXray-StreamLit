fd5cd949c78a44409385ce849f367adef7b647ac
JoshDumo <joshdumo@live.com>
2020-10-29 23:19:16 +0900

    clean up

--- chestX_v0.py
+++ chestX_v0.py
@@ -39,7 +39,7 @@ import shap
 from PIL import Image
 
 st.title("Chest X-ray Image Classification")
-st.text("This is an AI  system that analyzes chest X-ray images and suggests possible diagnoses.")
+st.text("This is an AI/ML system that analyzes chest X-ray images and suggests possible diagnoses.")
 st.subheader("Give it a go!")
 st.text("Upload a chest X-ray image on the side-bar and watch the ML magic happen!")
 st.text("[for now, only png format is allowed] ")
